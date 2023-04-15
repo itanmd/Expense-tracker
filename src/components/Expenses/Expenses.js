89d253e6d91cf60d0c636daa89e7fe1d789d7b66
Itan <itanmoshe@gmail.com>
2023-04-15 21:30:48 +0300

    ExpenseList comp & another way for conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -15,18 +15,7 @@ const filteredExpenses = props.items.filter(expense => {
   return expense.date.getFullYear().toString() === filteredYear;
 });
 
-let expensesContent = <p>No expenses found.</p>
 
-if(filteredExpenses.length > 0) {
-  expensesContent = filteredExpenses.map((expense) => (
-    <ExpenseItem 
-    key={expense.id}
-    title={expense.title} 
-    amount={expense.amount} 
-    date={expense.date}
-    />
-    ))
-}
 
   return (
     <div>
@@ -35,7 +24,7 @@ if(filteredExpenses.length > 0) {
       selected={filteredYear} 
       onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
   );
